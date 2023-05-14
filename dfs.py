911da35099154f59787b268577d7f21858292fe6
gompaang <hjhur004@gmail.com>
2023-05-14 14:51:17 +0900

    add dfs.py (recursive)

---
+++ dfs.py
@@ -0,0 +1,24 @@
+#DFS : recursive
+
+def dfs(graph, start, visited):
+    visited.append(start)
+    for node in graph[start]:
+        if node not in visited:
+            dfs(graph, node, visited)
+
+    return visited
+
+visited = []
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+print(dfs(graph, 1, visited))
